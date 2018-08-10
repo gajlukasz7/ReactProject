0dc3c4255253454383987fbef00b212ca863ad94
= <=>
2018-08-10 17:12:22 +0200

    + commit before eject

--- first_project/src/Components/news_list_item.js
+++ first_project/src/Components/news_list_item.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import {css} from 'glamor';
+import classes from '../css/styles.css';
 
 const NewsItem = ({item}) => {
 
-    const news_item = css({
-        padding: '20px',
-        boxSizing: 'border-box',
-        borderBottom: '1px solid grey',
-        ':hover': {
-            color:'red'
-        },
-        '@media(max-width: 500px)': {
-            color: 'blue'
-        }
-    })
-
-    const item_grey = css({
-        background: 'lightgrey'
-    })
-
     return(
-        <div className={`${news_item} ${item_grey}`}>
+        <div className={classes.new_item}>
             <h3>{item.title}</h3>
             <div>
                 {item.feed}
